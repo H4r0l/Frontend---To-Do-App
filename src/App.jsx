c762091009ec4cd704e6db7b6871a01872cf6477
Jajaharold <haroldurrea@gmail.com>
2023-02-12 17:24:41 -0500

    Basic Structure React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+
+    </div>
+  )
+}
+
+export default App
